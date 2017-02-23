df37b05431ea49e6cb1a6fbd1894a2f32ef633a7
GaynullinDima <dmitriy.gaynullin@phystech.edu>
2017-02-23 15:04:07 +0300

    fixed db

--- db.sql
+++ db.sql
@@ -1,25 +1,21 @@
 CREATE TABLE IF NOT EXISTS station (
-  id  PRIMARY KEY AUTO_INCREMENT,
-  station_name VARCHAR(255) NOT NULL DEFAULT '',
+  id SERIAL PRIMARY KEY, station_name VARCHAR(255) NOT NULL DEFAULT '',
   city VARCHAR(255) NOT NULL DEFAULT '',
-  coordinate POINT NOT NULL DEFAULT (0, 0)
+  coordinate POINT NOT NULL DEFAULT POINT(0, 0)
 );
-
 CREATE TABLE IF NOT EXISTS vehicle_type (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   vehicle_name VARCHAR(255) NOT NULL DEFAULT ''
 );
-
 CREATE TABLE IF NOT EXISTS route (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  id SERIAL PRIMARY KEY,
   route_name VARCHAR(255) NOT NULL DEFAULT '',
   start_station_id INTEGER REFERENCES station(id),
   final_station_id INTEGER REFERENCES station(id),
   vehicle_type_id INTEGER REFERENCES vehicle_type(id)
 );
-
 CREATE TABLE IF NOT EXISTS neighb_stations (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  id BIGSERIAL PRIMARY KEY,
   from_station_id INTEGER REFERENCES station(id),
   to_station_id INTEGER REFERENCES station(id),
   route_id INTEGER REFERENCES route(id),
@@ -27,4 +23,4 @@ CREATE TABLE IF NOT EXISTS neighb_stations (
   arrival_time TIME,
   price REAL,
   price_last_update_time TIMESTAMP
-);
+);	
